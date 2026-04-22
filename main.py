770369ff99ebd662dd2ae6f64475a6aca684bedf
{saurav} <{vsauravs2004@gmail.com}>
2026-04-22 21:18:33 +0530

    Added the structure for main file

---
+++ main.py
@@ -0,0 +1,8 @@
+from core.fifo import fifo
+from core.lru import lru
+from core.optimal import optimal
+from core.metrics import compare_algorithms
+print("\nFrame Variation")
+
+pages = [7, 0, 1, 2, 0, 3, 0, 4]
+frame_sizes = [2, 3, 4, 5]
