615e0d7dc4bbaf5d46451ebf3ca1be0a4d8b62bb
{saurav} <{vsauravs2004@gmail.com}>
2026-04-22 21:19:55 +0530

    Added the complete logic for main file

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ print("\nFrame Variation")
 
 pages = [7, 0, 1, 2, 0, 3, 0, 4]
 frame_sizes = [2, 3, 4, 5]
+
+for f in frame_sizes:
+    fifo_faults, _ = fifo(pages, f)
+    lru_faults, _ = lru(pages, f)
+    opt_faults, _ = optimal(pages, f)
+
+    print(f"\nFrames = {f}")
+    print(f"FIFO: {fifo_faults}, LRU: {lru_faults}, Optimal: {opt_faults}")
