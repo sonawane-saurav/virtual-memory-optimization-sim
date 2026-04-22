e83fddfbfe13c83e1a5429eeeed2a94b4775b162
Adhil0786 <adhilabdul.nasar2024@lpu.in>
2026-04-22 22:07:09 +0530

    Added the logic for metrics calculation and comparison

--- core/metrics.py
+++ core/metrics.py
@@ -4,8 +4,25 @@ Fault Rate = Page Faults / Total Pages
 Hit Rate = Hits / Total Pages
 '''
 
-def calculate_metrics():
+def calculate_metrics(pages, page_faults):
+    total = len(pages)
+    hits = total - page_faults
 
+    fault_rate = page_faults / total
+    hit_rate = hits / total
 
+    return {
+        "total_pages" : total,
+        "page_faults" : page_faults,
+        "hits" : hits,
+        "fault_rate" : fault_rate,
+        "hit_rate" : hit_rate
+    }
 
-def compare_algorithms():
+def compare_algorithms(results, pages):
+      comparison = {}
+
+    for name, faults in results.items():
+        comparison[name] = calculate_metrics(pages, faults)
+
+    return comparison
